96b3ed1b11ef06f04de9a8278e154638d4da24a8
xlan11 <alankelly@gmail.com>
2022-07-29 21:35:00 +0100

    Updates

---
+++ precache-manifest.bf7e75bac66e71213d571ee8aa76884f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07d72b42ada0f1257ee",
+    "url": "/business-card/static/css/main.e8b14ec8.chunk.css"
+  },
+  {
+    "revision": "c07d72b42ada0f1257ee",
+    "url": "/business-card/static/js/main.c07d72b4.chunk.js"
+  },
+  {
+    "revision": "8827cf04fdb1fc1434ff",
+    "url": "/business-card/static/js/1.8827cf04.chunk.js"
+  },
+  {
+    "revision": "f8523fcd0ece220ed42c",
+    "url": "/business-card/static/js/runtime~main.f8523fcd.js"
+  },
+  {
+    "revision": "d049a453c93972dc066dc07537bfcb60",
+    "url": "/business-card/static/media/robbie.d049a453.png"
+  },
+  {
+    "revision": "d7a13f63f8e5064671974c213023ab76",
+    "url": "/business-card/index.html"
+  }
+];
